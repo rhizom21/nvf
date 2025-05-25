d45582b0febde207daacec3cbccca4ed2589cdd0
2024 <mail@lukasrosier.com>
2025-05-25 21:43:00 +0200

    first commit

---
+++ nvf-conf.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, ... }:
+{
+  vim = {
+      theme = {
+        enable = true;
+	name = "gruvbox";
+	style = "dark";
+      };
+    statusline.lualine.enable = true;
+    telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
+
+    languages = {
+      enableLSP = true;
+      enableTreesitter = true;
+      
+      nix.enable = true;
+      html.enable = true;
+    };
+  };
+}
