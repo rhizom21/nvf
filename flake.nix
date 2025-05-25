be11c87f67ae6af6cea02d69257e7e8c3b394cd0
2024 <mail@lukasrosier.com>
2025-05-25 21:48:00 +0200

    aarch64

--- flake.nix
+++ flake.nix
@@ -5,33 +5,17 @@
   };
 
   outputs = {nixpkgs, ...} @ inputs: {
-    packages.x86_64-linux = {
+    packages.aarch64-linux = {
       # Set the default package to the wrapped instance of Neovim.
       # This will allow running your Neovim configuration with
       # `nix run` and in addition, sharing your configuration with
       # other users in case your repository is public.
       default =
         (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          pkgs = nixpkgs.legacyPackages.aarch64-linux;
           modules = [
 	    ./nvf-conf.nix
-          #  {
-          #    config.vim = {
-          #      # Enable custom theming options
-          #      theme.enable = true;
-
-          #      # Enable Treesitter
-          #      treesitter.enable = true;
-
-	  #      languages.rust.enable = true;
-	  #      languages.nix.enable = true;
-
-          #      # Other options will go here. Refer to the config
-          #      # reference in Appendix B of the nvf manual.
-          #      # ...
-          #    };
-          #  }
-          ];
+                    ];
         })
         .neovim;
     };
